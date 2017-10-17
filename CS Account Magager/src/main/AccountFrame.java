676932c88c0be37929c481e20a0a52db8fd119c5
Josh <janderson152481@gmail.com>
2017-10-17 10:45:02 -0600

    AccountFame and LoginPanel

---
+++ CS Account Magager/src/main/AccountFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class AccountFrame {
+
+public static void main(String[] args) {
+        
+        JFrame aFrame = new JFrame("CS-AccountManager");
+        aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LoginPanel Lpanel = new LoginPanel();
+        aFrame.getContentPane().add(Lpanel);
+        aFrame.pack();
+        aFrame.setVisible(true);
+        
+        
+
+    }
+}
