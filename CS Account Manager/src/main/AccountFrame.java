3eba853c414331721b4bc8c207158d2d7177ff2f
Auston Rogers <austonpowers@live.com>
2017-10-17 23:29:43 -0700

    Signed-off-by: Auston Rogers <austonpowers@live.com>

--- CS Account Manager/src/main/AccountFrame.java
+++ CS Account Manager/src/main/AccountFrame.java
@@ -2,16 +2,15 @@ import javax.swing.JFrame;
 
 public class AccountFrame {
 
-public static void main(String[] args) {
-        
-        JFrame aFrame = new JFrame("CS-AccountManager");
-        aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        LoginPanel Lpanel = new LoginPanel();
-        aFrame.getContentPane().add(Lpanel);
-        aFrame.pack();
-        aFrame.setVisible(true);
-        
-        
+ public static void main(String[] args) {
 
-    }
+  JFrame aFrame = new JFrame("CS-AccountManager");
+  aFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  LoginPanel Lpanel = new LoginPanel();
+  ViewAccountMember accountView = new ViewAccountMember();
+  aFrame.getContentPane().add(Lpanel);
+  aFrame.pack();
+  aFrame.setVisible(true);
+
+ }
 }
--- CS Account Manager/src/main/ViewAccountMember.java
+++ CS Account Manager/src/main/ViewAccountMember.java
@@ -1,6 +1,9 @@
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -8,20 +11,36 @@ import javax.swing.JPanel;
 public class ViewAccountMember extends JPanel {
  private AccountMember member;
  private JLabel displayName;
+ private JButton logOut;
 
  public ViewAccountMember() {
-  member = new AccountMember("Auston Rogers");
-  displayName = new JLabel(member.getName());
-  this.add(displayName);
-  this.setVisible(true);
+  member = new AccountMember("Auston Rogers");// initializes a new member
+  displayName = new JLabel(member.getName());// sets the label text to the member's name
+  logOut = new JButton("Log Out");// initializes the Log Out button
+  this.add(displayName);// add the label to the panel
+  this.add(logOut);// add the Log Out button to the panel
+  this.setVisible(true);// panel is visible
   this.setPreferredSize(new Dimension(500, 500));
+  logOut.addActionListener(new ButtonListener());
  }
 
- public static void main(String[] args) {
-  JFrame test = new JFrame();
-  test.setVisible(true);
-  test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  test.add(new ViewAccountMember());
-  test.pack();
+ private class ButtonListener implements ActionListener {
+
+  @Override
+  public void actionPerformed(ActionEvent arg0) {
+   // We will need to create a method that, when the logout button is pressed,
+   // transfers focus to the Login panel
+   System.out.println("Button pressed");
+  }
+
+ }
+
+ public static void main(String[] args) {// the main method below is for unit testing
+  JFrame testFrame = new JFrame();
+  ViewAccountMember x = new ViewAccountMember();
+  testFrame.getContentPane().add(x);
+  testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  testFrame.setVisible(true);
+  testFrame.pack();
  }
 }
